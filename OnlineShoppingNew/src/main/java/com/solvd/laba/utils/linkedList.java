73a5e28539651ef62234a6dc0596ab0747e3e46c
jishaGithub <131222172+jishaGithub@users.noreply.github.com>
2023-06-25 22:18:07 -0400

    Update linkedList.java

--- OnlineShoppingNew/src/main/java/com/solvd/laba/utils/linkedList.java
+++ OnlineShoppingNew/src/main/java/com/solvd/laba/utils/linkedList.java
@@ -4,19 +4,19 @@ class Node<T> {
     T data;
     Node<T> next;
 
-    Node(T data)
-    {
+    Node(T data) {
         this.data = data;
         this.next = null;
     }
 }
 
-
-public class linkedList<T> {
+public class LinkedList<T> {
     Node<T> head;
     private int length = 0;
 
-    public linkedList() { this.head = null; }
+    public LinkedList() {
+        this.head = null;
+    }
 
     public void add(T element) {
         Node<T> newNode = new Node<>(element);
@@ -32,41 +32,22 @@ public class linkedList<T> {
         }
         length++;
     }
-    /*
-    public void add(T data)
-    {
-        node<T> currentNode = new node<>(data);
-        if (this.head == null) {
-            head = currentNode;
-        }
-        else {
-            node<T> currentLL = head;
-            while (currentLL.next != null) {
-                currentLL = currentLL.next;           }
-            currentLL.next = currentNode;
-        }
-        length++;
-    }
-*/
-    public void remove(T removeNode)
-    {
+
+    public void remove(T removeNode) {
         Node<T> prev = new Node<>(null);
         prev.next = head;
         Node<T> next = head.next;
         Node<T> currentNode = head;
         boolean exists = false;
 
-        // If head node needs to be deleted
         if (head.data == removeNode) {
             head = head.next;
             exists = true;
         }
         while (currentNode.next != null) {
-            if (String.valueOf(currentNode.data).equals(
-                    String.valueOf(removeNode))) {
-
-                 prev.next = next;
-                 exists = true;
+            if (String.valueOf(currentNode.data).equals(String.valueOf(removeNode))) {
+                prev.next = next;
+                exists = true;
                 break;
             }
             prev = currentNode;
@@ -74,51 +55,42 @@ public class linkedList<T> {
             next = currentNode.next;
         }
 
-        if (exists == false && String.valueOf(currentNode.data).equals(
-                String.valueOf(removeNode))) {
+        if (!exists && String.valueOf(currentNode.data).equals(String.valueOf(removeNode))) {
             prev.next = null;
             exists = true;
         }
 
         if (exists) {
             length--;
-        }
-        else {
-            System.out.println("The given Value is not available");
+        } else {
+            System.out.println("The given value is not available");
         }
     }
 
-    public void clear()
-    {
+    public void clear() {
         head = null;
         length = 0;
     }
-    public boolean empty()
-    {
-        if (head == null) {
-            return true;
-        }
-        return false;
+
+    public boolean empty() {
+        return head == null;
     }
 
-    public int length() { return this.length; }
-
+    public int length() {
+        return this.length;
+    }
 
     public void display() {
+        Node<T> current = head;
 
-        Node current = head;
-
-        if(head == null) {
+        if (head == null) {
             System.out.println("Empty");
             return;
         }
 
-        while(current != null) {
+        while (current != null) {
             System.out.print(current.data + "\n");
             current = current.next;
-        }
-        System.out.println();
+        }        
     }
 }
-
-
