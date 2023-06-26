2dd65689173818ccd4dfee55dc009c4c503e7cff
jishaGithub <131222172+jishaGithub@users.noreply.github.com>
2023-06-25 23:26:31 -0400

    Update NotValidCardNoException.java

--- OnlineShoppingNew/src/main/java/com/solvd/laba/exceptions/NotValidCardNoException.java
+++ OnlineShoppingNew/src/main/java/com/solvd/laba/exceptions/NotValidCardNoException.java
@@ -3,6 +3,7 @@ package com.solvd.laba.exceptions;
 public class NotValidCardNoException extends Exception {
     private final String cardNo;
     private final String errorMsg;
+
     public NotValidCardNoException(String errorMsg, String cardNo) {
         this.errorMsg = errorMsg;
         this.cardNo = cardNo;
